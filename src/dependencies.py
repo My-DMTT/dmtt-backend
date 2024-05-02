96d86e2e194eb548f96e88665df5ef4332236d72
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-05-02 10:34:52 +0500

    depend upg

--- src/dependencies.py
+++ src/dependencies.py
@@ -47,11 +47,10 @@ async def get_current_user(token: str = Depends(reuseable_oauth)):
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="Could not find user",
             )
-
         return user
     except Exception as e:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
+            status_code=405,
             detail=f"Could not find user {str(e.args)} {str(e)}",
         )
 
