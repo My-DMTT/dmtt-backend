0029ec52a043d68f578d43eca3b5369a5a3eeb26
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-05-12 08:27:35 +0500

    fdg

--- src/api/controllers/limit_router.py
+++ src/api/controllers/limit_router.py
@@ -9,15 +9,14 @@ from src.services.limit_service import LimitService
 router = APIRouter()
 limit_service = LimitService()
 
-limitlar: Dict[str, Dict] = {}
+# limitlar: Dict[str, Dict] = {}
 
 
 @router.get("/limit")
 async def get_limit_dmtt(company_id: int, manager=Depends(get_current_manager)):
-    if company_id in limitlar:
-        return limitlar.get(company_id)
+
     data = await limit_service.get_limit_by_dmtt(
         manager=manager, company_id=company_id
     )
-    limitlar[company_id] = data
+
     return data
